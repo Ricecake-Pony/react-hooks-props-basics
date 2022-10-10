241f209a9f529f95fd81afc06fea4afc01277bf6
Trey <trey.delcamp@gmail.com>
2022-10-10 17:56:51 -0500

    commit tree 1c142c78bdfc14933d3367d06d519fb28056ae2b

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import BlogPost from "./BlogPost";
 
+function BlogContent(props) {
+  return <div>{props.articleText}</div>;
+}
+
 function App() {
   return <BlogPost />;
 }
